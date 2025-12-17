6c8d6d0644c694383ee08c8aa46dd3a6f7b8c03c
Vasiliy Vinogradov <vase3k@gmail.com>
2025-12-17 18:48:53 +0300

    feedback section - swiper

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -11,9 +11,18 @@ const swiperFeedback = new Swiper('.carousel', {
 		draggable: true,
 		dragClass: 'carousel__drag',
 		horizontalClass: 'carousel__scrollbar',
+		dragSize: 85,
 	},
 	initialSlide: 0,
 	spaceBetween: 32,
+	breakpoints: {
+		1024: {
+			width: 375,
+		},
+		1440: {
+			width: 528,
+		}
+	}
 });
 
 export default swiperFeedback;
